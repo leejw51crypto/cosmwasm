5a2a4a48dc6d9e2d9d56d232b484d5c482a56f5c
jonghwan <jonghwan@crypto.com>
2021-01-22 16:55:41 +0900

    fix code

--- myapple/upload.sh
+++ myapple/upload.sh
@@ -1,3 +1,3 @@
 . ./setup.sh
-myapplecli tx wasm store ./contract.wasm --from $S1 --gas-prices="0.025cosmos" --gas="auto" --gas-adjustment="1.2" --keyring-backend test --chain-id test  
-# myapplecli query wasm list-code
+myapplecli tx wasm store ./contract.wasm --from $S1 --gas-prices="0.025cosmos" --gas="auto" --gas-adjustment="1.2" --keyring-backend test --chain-id test
+myapplecli query wasm list-code
